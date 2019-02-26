4e9ff03640a3e50a51229f56c4c5d46b8e249273
Lemuel <lemuel.raganas@gmail.com>
2019-02-26 16:58:28 +0800

    WIP: Share URL panel

--- js/components/sharePanel.js
+++ js/components/sharePanel.js
@@ -38,12 +38,54 @@ var SharePanel = createReactClass({
       hasError: false
     };
   },
+    handleCheckboxClick: function(event) {
+        this.setState({shareAtTime: event.target.checked});
+    },
+
+    handleUrlClick: function(event) {
+        var target = event.target,
+            currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+        var succeed=false;
+        try {
+            succeed = document.execCommand("copy");
+        } catch(e) {
+            succeed = false;
+            target.removeAttribute('readonly');
+        }
+        if (currentFocus)
+            currentFocus.focus()
+    },
+
+    handleTimeChange: function(event) {
+        var el = event.target,
+            value = el.value;
+        var a = value.split(':');
+        var seconds = a.pop();
+        var minutes = a.length > 0 ? a.pop() : 0;
+        var hours = a.length > 0 ? a.pop() : 0;
+        var totalSeconds = ((+hours) * 60 * 60) + ((+minutes) * 60) + (+seconds);
+        if (totalSeconds > this.props.duration) {
+            totalSeconds=this.props.duration;
+        }
+
+        var params = {userPlayHeadTime: totalSeconds, shareAtTime: true}
+        this.setState(params);
+    },
 
   getActivePanel: function() {
+    var initialTime = isFinite(parseInt(this.props.currentPlayhead)) ? parseInt(this.props.currentPlayhead) : 0;
+    if ( this.state.userPlayHeadTime ) {
+        initialTime = parseInt(this.state.userPlayHeadTime);
+    }
+
+    var playheadTime = Utils.formatSeconds(initialTime);
+
     if (this.state.activeTab === this.tabs.SHARE) {
-      var socialContent = _.uniq(
-        Utils.getPropertyValue(this.props.skinConfig, 'shareScreen.socialContent', [])
-      );
+      var titleString = Utils.getLocalizedString(this.props.language, CONSTANTS.SKIN_TEXT.SHARE_CALL_TO_ACTION, this.props.localizableStrings);
+      var socialContent = _.uniq(Utils.getPropertyValue(this.props.skinConfig, 'shareScreen.socialContent', []));
+      var shareUrl = this.getShareLocation();
 
       var shareButtons = [];
       socialContent.forEach(function(shareButton) {
@@ -65,13 +107,32 @@ var SharePanel = createReactClass({
         }
       }, this);
 
-      return <div className="oo-share-tab-panel">{shareButtons}</div>;
-    } else if (this.state.activeTab === this.tabs.EMBED) {
+
+      return (
+        <div className="oo-share-tab-panel">
+          <div className="oo-social-action-text oo-text-capitalize">{titleString}</div>
+          {shareButtons}
+
+        <div className="share-url-text"><input type="url" readOnly value={shareUrl} onClick={this.handleUrlClick}/></div>
+        <label className="share-check-label">
+            <input type="checkbox" checked={this.state.shareAtTime} onChange={this.handleCheckboxClick} />
+            Share At
+        </label>
+        <input className="share-time-text" type="text" value={playheadTime} onChange={this.handleTimeChange} />
+        </div>
+      );
+    } 
+else if (this.state.activeTab === this.tabs.EMBED) {
       try {
         var iframeURL = this.props.skinConfig.shareScreen.embed.source
           .replace('<ASSET_ID>', this.props.assetId)
           .replace('<PLAYER_ID>', this.props.playerParam.playerBrandingId)
           .replace('<PUBLISHER_ID>', this.props.playerParam.pcode);
+
+if (this.state.shareAtTime) {
+			iframeURL=iframeURL.replace('&pcode=', '&options[initialTime]='+initialTime+'&view=embed&pcode=');
+		}
+
       } catch (err) {
         iframeURL = '';
       }
@@ -84,6 +145,38 @@ var SharePanel = createReactClass({
     }
   },
 
+  getShareLocation: function() {
+    var shareAtTime = this.state.shareAtTime || false;
+	if (shareAtTime) {
+	  var playheadTime = isFinite(parseInt(this.props.currentPlayhead)) ? parseInt(this.props.currentPlayhead) : '';
+	  if ( this.state.userPlayHeadTime ) {
+	    playheadTime = parseInt(this.state.userPlayHeadTime);
+	  }
+      var urlparser = document.createElement('a')
+      urlparser.href = window.videoInfo && window.videoInfo.url ? window.videoInfo.url : location.href;
+	  var qs= urlparser.search ? urlparser.search.substring(1).split('&')
+		.map(function(x){
+			return x.split('=',2).map(function(i){
+				return decodeURIComponent(i.trim()).replace('+', ' ');
+			})
+		})
+	   .reduce(function(m,x){m[x[0]]=x[1];return m},{}) : {};
+	  qs['t']=playheadTime;
+	  qs['autoplay']='1';
+      qs['view']='embed';
+	  var str=''
+	  for(var k in qs) {
+		if (str.length > 0) str += '&';
+		str += encodeURIComponent(k) + '=' + encodeURIComponent(qs[k]);
+	  }
+		console.info(['getShareLocation',urlparser.href]);
+	  return (urlparser.search ? urlparser.href.substring(0, urlparser.href.indexOf('?')) : urlparser.href) + '?' + str;
+	} else{
+		console.info(['getShareLocation',location.href]);
+	  return location.href;
+	}
+  },
+
   handleEmailClick: function(event) {
     event.preventDefault();
     var emailBody = Utils.getLocalizedString(
@@ -93,7 +186,7 @@ var SharePanel = createReactClass({
     );
     var mailToUrl = 'mailto:';
     mailToUrl += '?subject=' + encodeURIComponent(this.props.contentTree.title);
-    mailToUrl += '&body=' + encodeURIComponent(emailBody + location.href);
+    mailToUrl += '&body=' + encodeURIComponent(emailBody + this.getShareLocation());
     // location.href = mailToUrl; //same window
     //TODO: Add html5-common to html5-skin?
     if (OO.isIos && OO.isSafari) {
@@ -116,13 +209,13 @@ var SharePanel = createReactClass({
 
   handleFacebookClick: function() {
     var facebookUrl = 'http://www.facebook.com/sharer.php';
-    facebookUrl += '?u=' + encodeURIComponent(location.href);
+    facebookUrl += '?u=' + encodeURIComponent(this.getShareLocation());
     window.open(facebookUrl, 'facebook window', 'height=315,width=780');
   },
 
   handleGPlusClick: function() {
     var gPlusUrl = 'https://plus.google.com/share';
-    gPlusUrl += '?url=' + encodeURIComponent(location.href);
+    gPlusUrl += '?url=' + encodeURIComponent(this.getShareLocation());
     window.open(
       gPlusUrl,
       'google+ window',
@@ -133,7 +226,7 @@ var SharePanel = createReactClass({
   handleTwitterClick: function() {
     var twitterUrl = 'https://twitter.com/intent/tweet';
     twitterUrl += '?text=' + encodeURIComponent(this.props.contentTree.title + ': ');
-    twitterUrl += '&url=' + encodeURIComponent(location.href);
+    twitterUrl += '&url=' + encodeURIComponent(this.getShareLocation());
     window.open(twitterUrl, 'twitter window', 'height=300,width=750');
   },
 
